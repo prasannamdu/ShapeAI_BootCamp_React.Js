063edb90f3f8c1068090fc9478336a81a463c7da
prasannamdu <86569861+prasannamdu@users.noreply.github.com>
2021-06-28 00:57:49 +0530

    Add files via upload

---
+++ 4uhfc/src/components/Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div class ="note">
+      <h1>JAVASCRIPT AND REACT JS</h1>
+        <p>
+        A Basic web dev react js bootcamp 
+        </p>
+    </div>
+    
+  );
+}
+export default Info;
